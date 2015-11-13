53a35f43abfd86b0c6bf01835c59d2aa1e294377
Carine <pic.carine@gmail.com>
2015-11-13 18:03:58 +0000

    Basis: routing (login, logout, register), templates (layout, etc)

--- src/index.py
+++ src/index.py
@@ -16,6 +16,8 @@ def init(app):
         app.config['ip_address'] = config.get("config", "ip_address")
         app.config['port'] = config.get("config", "port")
         app.config['url'] = config.get("config", "url")
+        app.config['username'] = config.get("config", "username")
+        app.config['password'] = config.get("config", "password")
 
     except:
         print ('Could not read config from: '), config_location
@@ -25,6 +27,34 @@ def init(app):
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['username']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['password']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('home'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    return render_template('register.html', error=error)
+
+
 
 if __name__ == '__main__':
     init(app)
