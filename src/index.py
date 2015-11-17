9b7cc028a271846eed3bbeb00f10fa187d2cff5e
Carine <pic.carine@gmail.com>
2015-11-17 22:12:55 +0000

    Fix mistake on query_db function

--- src/index.py
+++ src/index.py
@@ -49,7 +49,8 @@ def init_db():
         db.commit()
 
 def query_db(query, args=(), one=False):
-    cur = g.db.execute(query, args)
+    db = get_db()
+    cur = db.execute(query, args)
     rv = [dict((cur.description[idx][0], value) for idx, value in enumerate(row)) for row in cur.fetchall()]
     return (rv[0] if rv else None) if one else rv
 
