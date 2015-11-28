47a94694e8d5cd67a977ac9a146d52dc5aa1ee24
Carine <pic.carine@gmail.com>
2015-11-28 21:12:06 +0000

    Add questions and answers table to SQL schema

--- src/schema.sql
+++ src/schema.sql
@@ -1,4 +1,6 @@
 DROP TABLE if EXISTS users;
+DROP TABLE if EXISTS questions;
+DROP TABLE if EXISTS answers;
 
 CREATE TABLE users (
     id integer PRIMARY KEY autoincrement,
@@ -7,3 +9,15 @@ CREATE TABLE users (
     email varchar(150) NOT NULL
     level varchar(50) NOT NULL DEFAULT "Beginner"
 );
+
+CREATE TABLE questions {
+    id integer PRIMARY KEY autoincrement,
+    question varchar(400) NOT NULL,
+    answer_id integer NOT NULL
+};
+
+CREATE TABLE answers {
+    id integer PRIMARY KEY autoincrement,
+    answer varchar(200) NOT NULL,
+    question_id integer NOT NULL
+};
